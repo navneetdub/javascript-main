aaa5163540a26e9a3a480e41e742e989dc043b65
Navneet Dhar Dubey <121310051+navneetdub@users.noreply.github.com>
2023-10-21 15:51:35 +0000

    datatypes

---
+++ .devcontainer/01_basics/01datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert("hello") //we are using node js not browser
+
+console.log("pintu");
+let name ="pintu"
+let age = 18
+let isloggedin= false
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
