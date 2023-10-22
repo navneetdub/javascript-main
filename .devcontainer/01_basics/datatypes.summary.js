46a621c7d60cc37d226ceea652a8d98da83ad907
Navneet Dhar Dubey <121310051+navneetdub@users.noreply.github.com>
2023-10-22 11:27:42 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes.summary.js
@@ -0,0 +1,20 @@
+//# Primitive
+
+//7 types: String, number,boolean,null,undefined,symbol,BigInt
+
+
+//# Reference Types(Non-Primitive)
+
+//Array, Objects, Functions
+
+const heroes=["shaktimaan", " naagraj", "doga"];
+let myObj={
+    name: "hitu",
+    age: 22;
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+//if you'll try to find the type of non primitive datatypes, you'll get object as a type
