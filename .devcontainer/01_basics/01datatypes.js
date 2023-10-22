3311f837949715362c33548ca7ee570a47fd1394
Navneet Dhar Dubey <121310051+navneetdub@users.noreply.github.com>
2023-10-22 10:57:48 +0000

    done

--- .devcontainer/01_basics/01datatypes.js
+++ .devcontainer/01_basics/01datatypes.js
@@ -43,3 +43,35 @@ let somenumber= 36
 let stringnumber= String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+//**************Operations**************** 
+
+let value=3
+let negvalue= -value
+console.log(negvalue);
+
+console.log(2+2);
+
+
+let str1="hello"
+let str2= " hitesh"
+
+let str3= str1+ str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"3");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter= 100
+gamecounter++;
+console.log(gamecounter);
