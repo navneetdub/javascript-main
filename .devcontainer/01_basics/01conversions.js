52eb36b87af4a6000a9c53b0e3b2fc1f3a9a2e32
Navneet Dhar Dubey <121310051+navneetdub@users.noreply.github.com>
2023-10-22 11:07:25 +0000

    hello

---
+++ .devcontainer/01_basics/01conversions.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2<1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+//you should better avoid these types of codes from 6 to 15
+
+
+// ===
+
+console.log("2" ===2 );
