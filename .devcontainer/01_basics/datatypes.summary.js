358204a8a6492d0e57d081f4a91724286d5d02a4
Navneet Dhar Dubey <121310051+navneetdub@users.noreply.github.com>
2023-10-24 05:49:25 +0000

    done

--- .devcontainer/01_basics/datatypes.summary.js
+++ .devcontainer/01_basics/datatypes.summary.js
@@ -10,7 +10,7 @@
 const heroes=["shaktimaan", " naagraj", "doga"];
 let myObj={
     name: "hitu",
-    age: 22;
+    age: 22
 }
 
 const myfunction = function(){
@@ -18,3 +18,28 @@ const myfunction = function(){
 }
 
 //if you'll try to find the type of non primitive datatypes, you'll get object as a type
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack Memory(Primitive), Heap Memory(Non-Primitive)
+
+let myYoutubename="navneetdhardubey"
+let anothername= myYoutubename
+anothername="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let user1= {
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+
+let user2= user1
+
+user2.email="navneet@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
